74e4cadc94f728b39d4157b7ff1ef0e46c1b818e
Tova Sauer <sauertova1@gmail.com>
2024-03-07 20:11:44 -0500

    added classes, index, professors, students, subjects, class_students, class_subjects

--- server/api/students.js
+++ server/api/students.js
@@ -1,27 +1,78 @@
-const router = require('express').Router();
-const {pool} = require('../db/index')
+const router = require("express").Router();
+const { pool } = require("../db/index");
 module.exports = router;
 
-//Route
-router.get('/', async (req,res,next) =>{
-    try{
-        const { rows } = await pool.query(`SELECT * FROM students`);
-        console.log('Query Result: ', rows)
-        res.json(allStudents);
-    } catch(err){
-        console.error(err);
-        res.status(500).send('Server Error');
-    }
+//Get all students
+router.get("/", async (req, res, next) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM students");
+    console.log("Query Results: ", rows);
+    res.json(rows);
+  } catch (err) {
+    console.error("Error with all students: ", err);
+    res.status(500).send("Server Error");
+  }
 });
 
-//Get single student localhost:1338/api/students/id
-router.get('/:id', async (req,res,next) => {
-    try {
-        const {id} = req.params; //const id  = req.params.id
-        const singleStudent = await pool.query(`SELECT * FROM students WHERE id = ${id}`);
-        res.json(singleStudent.rows)
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error with single student')
-    }
+//Get individual student
+router.get("/:student_id", async (req, res, next) => {
+  try {
+    const { student_id } = req.params;
+    const singleStudent = await pool.query(
+      `SELECT * FROM students WHERE student_id = ${student_id}`
+    );
+    res.json(singleStudent.rows);
+  } catch (err) {
+    console.error("Error with individual student: ", err);
+    res.status(500).send("Server Error");
+  }
+});
+
+//Create student
+router.post("/", async (req, res, next) => {
+  try {
+    const { fname, lname, email, birthdate } = req.body;
+    const newStudent = await pool.query(
+      `INSERT INTO students (fname, lname, email, birthdate) VALUES
+($1, $2, $3, $4) RETURNING *`,
+      [fname, lname, email, birthdate]
+    );
+    console.log(newStudent);
+    res.status(201).json(newStudent.rows[0]);
+  } catch (err) {
+    console.error("Error creating student: ", err);
+    res.status(500).send("Server Error");
+  }
+});
+
+//Edit student
+router.put("/:student_id", async (req, res, next) => {
+  try {
+    const { student_id } = req.params;
+    const { fname, lname, email, birthdate } = req.body;
+    const updatedStudent = await pool.query(
+      `UPDATE students SET fname = $2, lname = $3, email = $4,
+birthdate = $5 WHERE student_id = $1`,
+      [student_id, fname, lname, email, birthdate]
+    );
+    res.status(201).json({
+      message: `Student ${student_id} successfully updated`,
+      data: updatedStudent.rows[0],
+    });
+  } catch (err) {
+    console.error("Error updating student: ", err);
+    res.status(500).send("Server Error");
+  }
+});
+
+//Delete student
+router.delete("/:student_id", async (req, res, next) => {
+  try {
+    const { student_id } = req.params;
+    await pool.query(`DELETE FROM students WHERE student_id = ${student_id}`);
+    res.json(`Student ${student_id} successfully deleted`);
+  } catch (err) {
+    console.error("Error deleting student: ", err);
+    res.status(500).send("Server Error");
+  }
 });
